148635842ed03c38ffa877ed7903264a26fa6725
Tom Tate <tomtate101@yahoo.com>
2025-06-27 15:52:51 -0700

    add image file

--- script.js
+++ script.js
@@ -9,11 +9,9 @@ function Book(title, author, pages, isRead, imageURL) {
     this.isRead = isRead
     this.imageURL = imageURL;
     this.ID = crypto.randomUUID(); //creates unique random ID for each book
-
 }
 
 const myLibrary = [];
-
 
 const MARK_READ_SVG_PATH = "M16.75 22.16L14 19.16L15.16 18L16.75 19.59L20.34 16L21.5 17.41L16.75 22.16M6 22C4.89 22 4 21.1 4 20V4C4 2.89 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V13.34C19.37 13.12 18.7 13 18 13C14.69 13 12 15.69 12 19C12 20.09 12.29 21.12 12.8 22H6Z";
 const MARK_UNREAD_SVG_PATH = "M12 21.5C10.65 20.65 8.2 20 6.5 20C4.85 20 3.15 20.3 1.75 21.05C1.65 21.1 1.6 21.1 1.5 21.1C1.25 21.1 1 20.85 1 20.6V6C1.6 5.55 2.25 5.25 3 5C4.11 4.65 5.33 4.5 6.5 4.5C8.45 4.5 10.55 4.9 12 6C13.45 4.9 15.55 4.5 17.5 4.5C18.67 4.5 19.89 4.65 21 5C21.75 5.25 22.4 5.55 23 6V20.6C23 20.85 22.75 21.1 22.5 21.1C22.4 21.1 22.35 21.1 22.25 21.05C20.85 20.3 19.15 20 17.5 20C15.8 20 13.35 20.65 12 21.5M12 8V19.5C13.35 18.65 15.8 18 17.5 18C18.7 18 19.9 18.15 21 18.5V7C19.9 6.65 18.7 6.5 17.5 6.5C15.8 6.5 13.35 7.15 12 8M13 11.5C14.11 10.82 15.6 10.5 17.5 10.5C18.41 10.5 19.26 10.59 20 10.78V9.23C19.13 9.08 18.29 9 17.5 9C15.73 9 14.23 9.28 13 9.84V11.5M17.5 11.67C15.79 11.67 14.29 11.93 13 12.46V14.15C14.11 13.5 15.6 13.16 17.5 13.16C18.54 13.16 19.38 13.24 20 13.4V11.9C19.13 11.74 18.29 11.67 17.5 11.67M20 14.57C19.13 14.41 18.29 14.33 17.5 14.33C15.67 14.33 14.17 14.6 13 15.13V16.82C14.11 16.16 15.6 15.83 17.5 15.83C18.54 15.83 19.38 15.91 20 16.07V14.57Z";
@@ -91,7 +89,6 @@ function handleBookButtons(e){
         editBook(e);
     else if (button.className === "remove")
         removeBook(e);
-
 }; 
 
 function addBookToLibrary(title, author, pages, isRead, imageURL) {
@@ -224,7 +221,6 @@ function toggleRead(event){
 function editBook(event){
     const card = event.target.closest(".card");
     const bookObj = getBookById(card.dataset.id);
-    console.log(bookObj);
 
     const editModal = document.querySelector("#editModal");
     editModal.dataset.bookId = bookObj.ID;
@@ -241,7 +237,6 @@ function editBook(event){
     newImageURL.value = bookObj.imageURL;
 
     editModal.showModal();
-    console.log("edit");
 }
 
 function removeBook(event){
